a924d63cf5ede696ea4cd6d9bbe34cb16ca7c47b
norkitorki <norki@tuta.io>
2022-06-28 07:31:29 +0200

    Add name,username columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_27_134018) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_052437) do
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", null: false
@@ -30,8 +30,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_27_134018) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
