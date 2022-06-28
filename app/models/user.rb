58d29e7ff43115216d191656ea13a6523b6fed82
norkitorki <norki@tuta.io>
2022-06-28 13:48:01 +0200

    Finish up the guide

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :username, :email, uniqueness: true, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
